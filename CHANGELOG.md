79a345f311a0b65edbbbadac90e921228f8cc164
jonathan-robertson <jrob356@icloud.com>
2024-07-12 19:27:24 -0500

    trigger distribution on p2p host

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,8 +12,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
   - [ ] patch `public override void UpgradeDowngradeFrom(TileEntityComposite _other)` to apply the *repairer* as the owner if the previous block has tag `roboticinboxinsecure`
   - [ ] or... maybe flush owner if downgrading *to* block with tag `roboticinboxinsecure`... will system substitute the new block owner on repair at that point?
 - [ ] add support for local and p2p, if possible
-  - [ ] trigger distribution on host
-  - [ ] trigger sign update on host
+  - [x] trigger distribution on p2p host
+  - [ ] trigger sign update on p2p host
 - [x] fix compilation errors
 - [x] fix recipe: value -> count in 1.0
 - [x] fix trader category -> match workbench
---
+++ src/Patches/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using System;
+
+namespace RoboticInbox.Patches
+{
+    [HarmonyPatch(typeof(GameManager), "TEUnlockServer")]
+    internal class GameManager_TEUnlockServer_Patches
+    {
+        private static readonly ModLog<GameManager_TEUnlockServer_Patches> _log = new ModLog<GameManager_TEUnlockServer_Patches>();
+
+        public static void Postfix(int _clrIdx, Vector3i _blockPos)
+        {
+            try
+            {
+                if (!ConnectionManager.Instance.IsServer)
+                {
+                    return;
+                }
+                StorageManager.Distribute(_clrIdx, _blockPos);
+            }
+            catch (Exception e)
+            {
+                _log.Error("Postfix", e);
+            }
+        }
+    }
+
+
+    [HarmonyPatch(typeof(GameManager), "TELockServer")]
+    internal class GameManager_TELockServer_Patches
+    {
+        private static readonly ModLog<GameManager_TELockServer_Patches> _log = new ModLog<GameManager_TELockServer_Patches>();
+
+        public static void Postfix(int _clrIdx, Vector3i _blockPos)
+        {
+            try
+            {
+                if (!ConnectionManager.Instance.IsServer)
+                {
+                    return;
+                }
+                if (StorageManager.ActiveCoroutines.TryGetValue(_blockPos, out var coroutine))
+                {
+                    _log.Trace($"Active coroutine detected at {_blockPos}; stopping and removing it.");
+                    _ = StorageManager.ActiveCoroutines.Remove(_blockPos);
+                    ThreadManager.StopCoroutine(coroutine);
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error("Postfix", e);
+            }
+        }
+    }
+}
