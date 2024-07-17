f416908bc98b860e4e8cdcd2ddb97a877b2ff5fe
jonathan-robertson <jrob356@icloud.com>
2024-07-17 05:27:36 -0500

    improve/simplify patches to work on and offline

--- src/Patches/GameManager.cs
+++ src/Patches/GameManager.cs
@@ -1,10 +1,11 @@
 ﻿using HarmonyLib;
 using RoboticInbox.Utilities;
 using System;
+using System.Collections.Generic;
 
 namespace RoboticInbox.Patches
 {
-    [HarmonyPatch(typeof(GameManager), "OnApplicationQuit")]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.OnApplicationQuit))]
     internal class GameManager_OnApplicationQuit_Patches
     {
         private static readonly ModLog<GameManager_OnApplicationQuit_Patches> _log = new ModLog<GameManager_OnApplicationQuit_Patches>();
@@ -24,7 +25,7 @@ namespace RoboticInbox.Patches
         }
     }
 
-    [HarmonyPatch(typeof(GameManager), "TEUnlockServer")]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.TEUnlockServer))]
     internal class GameManager_TEUnlockServer_Patches
     {
         private static readonly ModLog<GameManager_TEUnlockServer_Patches> _log = new ModLog<GameManager_TEUnlockServer_Patches>();
@@ -47,7 +48,7 @@ namespace RoboticInbox.Patches
     }
 
 
-    [HarmonyPatch(typeof(GameManager), "TELockServer")]
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.TELockServer))]
     internal class GameManager_TELockServer_Patches
     {
         private static readonly ModLog<GameManager_TELockServer_Patches> _log = new ModLog<GameManager_TELockServer_Patches>();
@@ -71,6 +72,58 @@ namespace RoboticInbox.Patches
             {
                 _log.Error("Postfix", e);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.ChangeBlocks))]
+    internal class GameManager_ChangeBlocks_Patches
+    {
+        private static readonly ModLog<GameManager_ChangeBlocks_Patches> _log = new ModLog<GameManager_ChangeBlocks_Patches>();
+
+        private static bool Prefix(PlatformUserIdentifierAbs persistentPlayerId, ref List<BlockChangeInfo> _blocksToChange, GameManager __instance)
+        {
+            try
+            {
+                if (!ConnectionManager.Instance.IsServer)
+                {
+                    return true;
+                }
+
+                for (var i = 0; i < _blocksToChange.Count; i++)
+                {
+                    var blockChangeInfo = _blocksToChange[i];
+                    if (!blockChangeInfo.blockValue.ischild
+                        && !blockChangeInfo.blockValue.isair
+                        && blockChangeInfo.bChangeBlockValue
+                        && StorageManager.HasRoboticInboxSecureTag(blockChangeInfo.blockValue.Block))
+                    {
+                        var tileEntity = __instance.m_World.GetTileEntity(blockChangeInfo.pos);
+                        if (tileEntity != null
+                            && tileEntity is TileEntityComposite composite
+                            && StorageManager.HasRoboticInboxInsecureTag(tileEntity.blockValue.Block))
+                        {
+                            // We can see that the block is being upgraded from insecure -> secure
+                            // i.e. our lock is being repaired
+                            if (persistentPlayerId == null) // host
+                            {
+                                _log.Trace($"[{blockChangeInfo.pos}] {__instance.persistentLocalPlayer?.PrimaryId?.CombinedString} repaired and has taken ownership over robotic inbox");
+                                composite.Owner = __instance.persistentLocalPlayer?.PrimaryId;
+                            }
+                            else // client/remote player
+                            {
+                                _log.Trace($"[{blockChangeInfo.pos}] {persistentPlayerId.CombinedString} repaired and has taken ownership over robotic inbox");
+                                composite.Owner = persistentPlayerId; // one repairing now takes ownership of block
+                            }
+                            composite.SetModified(); // sync change to clients
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error("Postfix", e);
+            }
+            return true;
         }
     }
 }
